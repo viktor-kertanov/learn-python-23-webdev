f48c95282a1f44186e5b419b183c33f911c9b213
Viktor Kertanov <thelonius19@gmail.com>
2022-01-15 12:03:40 +0300

    Remember user checkebox and functionality implemented

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import BooleanField, StringField, PasswordField, SubmitField
 from wtforms import validators
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField("Имя пользователя", validators=[DataRequired()], render_kw={"class": "form-control my-3"})
     password = PasswordField("Пароль", validators=[DataRequired()], render_kw={"class": "form-control mb-3"})
+    remember_me = BooleanField("Запомнить меня", default=True, render_kw={"class": "form-check-input"})
     submit = SubmitField("Отправить!", render_kw={"class": "btn btn-primary"})
